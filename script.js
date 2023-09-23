d66ea20252b0b34d12d82538918ec407f558fa75
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2023-09-23 10:30:39 -0500

    deployment

--- script.js
+++ script.js
@@ -1,31 +1,78 @@
-const submitEl = document.querySelector(".submit-button");
-const formWrapper = document.querySelector(".formWrapper");
-const messageEl = document.querySelector('.message');
-const alert = document.createElement("div");
-const dateInput = document.querySelector(".date");
-const weightInput = document.querySelector(".weight");
-const measurmentInput = document.querySelector(".measurement");
+const weightArray = JSON.parse(localStorage.getItem('weightArray')) || [];
 
-let errorMessageShown = false;
+const weightForm = document.getElementById('weightForm');
+const table = document.getElementById('weightTable');
 
-submitEl.addEventListener("click", (event) => {
+window.addEventListener('load', (e) => {
+  e.preventDefault();
+  weightArray.forEach(weightEntry => {
+    submitEntry(weightEntry);
+  })
+})
+
+weightForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (!errorMessageShown && (!dateInput.value || !weightInput.value || !measurmentInput.value)) {
-    alert.classList.add("alert");
-    alert.textContent = "Please enter data in all fields!";
-    messageEl.appendChild(alert)
-    errorMessageShown =  true;
-  } else {
-    submitEntry();
-  }
-});
-
-function submitEntry () {
-  if (errorMessageShown && dateInput.value && weightInput.value && measurmentInput.value) {
-     messageEl.removeChild(alert);
-     document.querySelector('.formEl').reset();
-  } 
-  errorMessageShown =  false;
   
+  const date = document.getElementById('date');
+  const weight = document.getElementById('weight');
+  const unit = document.getElementById('unit');
+
+  const newWeight = {
+    id: Math.random(),
+    date: date.value,
+    weight: weight.value, 
+    unit: unit.value
+  }
+
+  submitEntry(newWeight);
+  weightArray.push(newWeight);
+  pushToLocalStorage(newWeight);
+})
+
+function submitEntry(newWeight) {
+  
+  const row = document.createElement('tr');
+
+  table.appendChild(row);
+
+  const dateEntry = document.createElement('td');
+  dateEntry.textContent = newWeight.date;
+  row.appendChild(dateEntry);
+
+  const weightEntry = document.createElement('td');
+  weightEntry.textContent = newWeight.weight;
+  row.appendChild(weightEntry);
+
+  const unitEntry = document.createElement('td');
+  unitEntry.textContent = newWeight.unit
+  row.appendChild(unitEntry);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.classList.add('delete-button');
+  row.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click', () =>{
+    deleteEntry(deleteButton, newWeight.id);
+  })
+
+  table.style.display = 'table';
+  weightForm.reset();
 }
 
+function deleteEntry(deleteButton, id) {
+  deleteButton.parentElement.remove();
+  for (let i = 0; i < weightArray.length; i++) {
+    if (weightArray[i].id === id) {
+      weightArray.splice(i, 1);
+      localStorage.setItem('weightArray', JSON.stringify(weightArray));
+    }
+  }
+  if (weightArray.length === 0) {
+    table.style.display = 'none';
+  }
+}
+
+function pushToLocalStorage(newWeight) {
+  localStorage.setItem('weightArray', JSON.stringify(weightArray));
+}
